aee83ff25482796f61445aac065de81d9300a79c
Aditi_Pandey <54734131+AditiThirdEye@users.noreply.github.com>
2021-04-30 22:10:21 +0530

    Semi Front End Part
    Aditi

---
+++ Disease/frontenddisease.py
@@ -0,0 +1,94 @@
+import numpy as np
+import pandas as pd
+import requests
+import pickle
+import io
+import torch
+from torchvision import transforms
+from PIL import Image
+from utils.model import ResNet9
+
+
+
+disease_classes = ['Apple___Apple_scab',
+                   'Apple___Black_rot',
+                   'Apple___Cedar_apple_rust',
+                   'Apple___healthy',
+                   'Blueberry___healthy',
+                   'Cherry_(including_sour)___Powdery_mildew',
+                   'Cherry_(including_sour)___healthy',
+                   'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot',
+                   'Corn_(maize)___Common_rust_',
+                   'Corn_(maize)___Northern_Leaf_Blight',
+                   'Corn_(maize)___healthy',
+                   'Grape___Black_rot',
+                   'Grape___Esca_(Black_Measles)',
+                   'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)',
+                   'Grape___healthy',
+                   'Orange___Haunglongbing_(Citrus_greening)',
+                   'Peach___Bacterial_spot',
+                   'Peach___healthy',
+                   'Pepper,_bell___Bacterial_spot',
+                   'Pepper,_bell___healthy',
+                   'Potato___Early_blight',
+                   'Potato___Late_blight',
+                   'Potato___healthy',
+                   'Raspberry___healthy',
+                   'Soybean___healthy',
+                   'Squash___Powdery_mildew',
+                   'Strawberry___Leaf_scorch',
+                   'Strawberry___healthy',
+                   'Tomato___Bacterial_spot',
+                   'Tomato___Early_blight',
+                   'Tomato___Late_blight',
+                   'Tomato___Leaf_Mold',
+                   'Tomato___Septoria_leaf_spot',
+                   'Tomato___Spider_mites Two-spotted_spider_mite',
+                   'Tomato___Target_Spot',
+                   'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
+                   'Tomato___Tomato_mosaic_virus',
+                   'Tomato___healthy']
+
+disease_model_path = 'utils/plant_disease_model.pth'
+disease_model = ResNet9(3, len(disease_classes))
+disease_model.load_state_dict(torch.load(disease_model_path, map_location=torch.device('cpu')))
+disease_model.eval()
+
+def predict_image(img, model=disease_model):
+    transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.ToTensor(),
+    ])
+    image = Image.open(io.BytesIO(img))
+    img_t = transform(image)
+    img_u = torch.unsqueeze(img_t, 0)
+
+    # Get predictions from model
+    yb = model(img_u)
+    # Pick index with highest probability
+    _, preds = torch.max(yb, dim=1)
+    prediction = disease_classes[preds[0].item()]
+    # Retrieve the class label
+    return prediction
+
+from io import BytesIO
+file = Image.open('DataDiseaseDetect/Apple___Black_rot/AppleBlackRot1.JPG')
+# Create a buffer to hold the bytes
+buf = BytesIO()
+
+# Save the image as jpeg to the buffer
+file.save(buf, 'jpeg')
+
+# Rewind the buffer's file pointer
+buf.seek(0)
+
+# Read the bytes from the buffer
+img = buf.read()
+
+# Close the buffer
+buf.close()
+
+
+prediction = predict_image(img)
+print(prediction)
+
